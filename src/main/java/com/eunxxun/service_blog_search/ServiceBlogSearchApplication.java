ff29d2ef96636c111a93d9e0d7f52ea91c57dc3f
박은선 <esun19@kakao.com>
2023-03-18 19:35:07 +0900

    프로젝트 초기 셋팅

---
+++ src/main/java/com/eunxxun/service_blog_search/ServiceBlogSearchApplication.java
@@ -0,0 +1,13 @@
+package com.eunxxun.service_blog_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBlogSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBlogSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eunxxun/service_blog_search/ServiceBlogSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunxxun.service_blog_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBlogSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
